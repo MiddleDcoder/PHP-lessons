f5b75fad3b2f95d1aefa62cd733a18cc3f4ac30f
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2019-05-14 16:55:35 +0800

    post method

---
+++ index.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>PHP lessons</title>
+    </head>
+    <body>
+        <form action="process.php" method="post">
+            <input name="name" type="text">
+            <input type="submit">
+        </form>
+    <?php 
+    //echo "Hello PHP !";
+
+    // $myVar = "This is my variable";
+
+    // echo $myVar;
+
+    // $name = "Jhun";
+
+    // echo "Hello, " . $name;
+   
+
+    ?>
+    </body>
+</html>
---
+++ process.php
@@ -0,0 +1,14 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>PHP lessons</title>
+    </head>
+    <body>
+ <?php
+ 
+ $name = $_POST["name"];
+ echo "Hello, ". $name;
+ 
+ ?>
+    </body>
+</html>
